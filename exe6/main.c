470edc8660bcc823bd28361f2fb1dd69bc994b29
Helena-Mansour <helenasm@al.insper.edu.br>
2026-02-22 11:54:50 -0300

    correção ex6

--- exe6/main.c
+++ exe6/main.c
@@ -7,7 +7,7 @@ const int FIRST_GPIO = 2;
 const int BTN_PIN_G = 28;
 
 
-int last_btn= 1; // Button not pressed (pulled up)
+
 
 // This array converts a number 0-9 to a bit pattern to send to the GPIOs
 int bits[10] = {
